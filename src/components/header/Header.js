96b0d47a5c92d3290a00f59059ceecc8d216d3c9
Stepanov Max <badsanta2010@gmail.com>
2019-06-29 17:54:07 +0300

    Third

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,12 @@
+import Menu from '../menu/Menu';
 import React from 'react';
 import classes from './Header.module.css';
 
 const header = (props) => {
-
   return (
     <div className={classes.Header}>
       Header
+      <Menu />
     </div>
   )
 }
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import classes from './Menu.module.css';
 
-const header = (props) => {
-  
+const menu = (props) => {
   return (
     <ul className={classes.Menu}>
       <li className={classes.MenuItem}>
-        Главная
+        <a href="" className={classes.MenuLink}>
+          Главная
+        </a>
+      </li>
+      <li className={classes.MenuItem}>
+        <a href="" className={classes.MenuLink}>
+          Контакты
+        </a>
       </li>
     </ul>
   )
 }
 
-export default header;
+export default menu;
